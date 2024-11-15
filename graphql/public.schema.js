b019dd8575b93d9d6bcc2a5453e5d9b659b436dc
navid bahrami <navidbahrami1996@gmail.com>
2024-11-15 12:48:51 +0330

    public schema

---
+++ graphql/public.schema.js
@@ -0,0 +1,24 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+const AnswerSchema = new mongoose.Schema({
+    user : {type : mongoose.Types.ObjectId , ref : 'user' , required : true} ,
+    comment : {type : String , required : true} ,
+    show : {type : Boolean , required : true , default : false}
+} , {
+    timestamps : {createdAt : true}
+})
+
+const commentSchema = new mongoose.Schema({
+    user : {type : mongoose.Types.ObjectId , ref : 'user' , required : true} ,
+    comment : {type : String , required : true} ,
+    show : {type : Boolean , required : true , default : false} ,
+    openToComment: {type : Boolean , default : true} ,
+    answer : {type : [AnswerSchema] , default : []}
+} , {
+    timestamps : {createdAt : true}
+})
+
+
+module.exports = {
+    commentSchema
+}
