6d95416b0d0db8e1fe66949f2553e063730a90d9
navid bahrami <navidbahrami1996@gmail.com>
2024-11-16 17:12:25 +0330

    public schema

--- graphql/public.schema.js
+++ graphql/public.schema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const AnswerSchema = new mongoose.Schema({
     user : {type : mongoose.Types.ObjectId , ref : 'user' , required : true} ,
     comment : {type : String , required : true} ,
-    show : {type : Boolean , required : true , default : false}
+    show : {type : Boolean , required : true , default : false} ,
+    openToComment: {type : Boolean , default : false} 
 } , {
     timestamps : {createdAt : true}
 })
