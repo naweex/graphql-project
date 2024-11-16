cee48a380a2867b4b63f4c57011e2d7eadadc08d
navid bahrami <navidbahrami1996@gmail.com>
2024-11-16 17:15:02 +0330

    user model

---
+++ graphql/models/users.js
@@ -0,0 +1,30 @@
+const Joi = require("joi");
+const { default: mongoose } = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    first_name : {type : String} ,
+    last_name : {type : String} ,
+    username : {type : String , lowercase: true} ,
+    mobile : {type : String , required : true } ,
+    email : {type : String , lowercase : true} ,
+    password : {type : String} ,
+    otp : {type : Object , default : {
+        code : 0 ,
+        expiresIn : 0
+    }} ,
+    bills : {type : [] , default : []} ,
+    discount : {type : Number , default : 0} ,
+    birthday : {type : String} ,
+    role  : {type : String , default : 'USER'} ,
+    courses : {type : [mongoose.Types.ObjectId] , ref : 'course' , default : []}
+
+}, {
+    timestamps : true ,
+    toJSON : {
+        virtuals : true
+    }
+});
+UserSchema.index({first_name : 'text' , last_name : 'text' , username : 'text' , mobile: 'text' , email : 'text'})
+module.exports = {
+    UserModel : mongoose.model('user' , UserSchema)
+}
